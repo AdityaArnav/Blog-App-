44cd9d199088b6c88fde546f0f79ec390505ab46
AdityaArnav <99977930+AdityaArnav@users.noreply.github.com>
2022-11-01 23:50:51 +0530

    managed state in login page(frontend)

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 const Auth = () => {
-  return (
-    <div>Auth</div>
-  )
-}
+    const [inputs, setInputs] = useState({
+        name : "",
+        email :"",
+        password : ""
+    })
+    const [isSignup, setIsSignup]= useState(false);
 
-export default Auth
+    const handleChange = (e) =>{
+        const {name, value} = e.target;
+        setInputs({...inputs,[name]:value})
+    }
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(inputs)
+
+    }
+    
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <Box
+          display="flex"
+          flexDirection={"column"}
+          alignItems="center"
+          justifyContent={"center"}
+          boxShadow="10px 10px 20px #ccc"
+          padding={3}
+          margin="auto"
+          marginTop={5}
+          borderRadius={5}
+          maxWidth={400}
+
+        >
+          <Typography padding={3} variant='h4' textAlign="center">{isSignup?"REGISTER":"LOGIN"}</Typography>
+          {isSignup && <TextField name="name" onChange={handleChange} placeholder='Name' margin="normal" />}
+          <TextField  name="email" type={"email"} onChange={handleChange} placeholder='Email' margin="normal" />
+          <TextField  name="password" type={'password'} onChange={handleChange} placeholder='Password' margin="normal" />
+          <Button variant='contained' sx={{borderRadius:3,marginTop:3}} color="warning" type="submit" >Submit</Button>
+          <Button onClick={()=>setIsSignup(isSignup===false?true:false)} sx={{borderRadius:3}} >Change to {isSignup?"Login":"Signup"}</Button>
+        </Box>
+      </form>
+    </>
+  );
+};
+
+export default Auth;
